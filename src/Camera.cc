4dfbffdab26395bff4b5ca52e9097909a0d66bee
HarryJackson1996 <harryjackson001@hotmail.co.uk>
2017-01-13 18:56:54 +0000

    can compile without errors
    working on implementing camera in further detail

--- src/Camera.cc
+++ src/Camera.cc
@@ -1,13 +1,17 @@
 #include "Camera.h"
 
-Camera:: ~Camera(){
-
+Camera::Camera(){
 }
 
-virtual Camera:: ~Camera(){
-
+Camera::~Camera(){
 }
 
 glm::mat4 getViewMatrix(){
-	return glm::mat4;
+	return glm::mat4();{
+
+                   glm::vec4(0.0f, 0.0f, 3.0f, 0.0f), 
+  		   glm::vec4(0.0f, 0.0f, 0.0f, 0.0f),
+	   	   glm::vec4(0.0f, 0.0f, 0.0f, 0.0f), 
+  		   glm::vec4(0.0f, 1.0f, 0.0f, 0.0f);
+}
 }
