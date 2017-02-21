cd728bdd15ad0e1243bea47e90f79417f60c2c67
HarryJackson1996 <harryjackson001@hotmail.co.uk>
2017-02-21 13:06:12 +0000

    Added the BoundingBox class

---
+++ src/BoundingBox.h
@@ -0,0 +1,22 @@
+#ifdef BOUNDINGBOX_H
+#define BOUNDINGBOX_H
+
+#include "GameAsset.h"
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+
+
+class BoundingBox : public GameAsset {
+ public:
+	
+
+
+
+
+ private:
+};
+
+#endif 
