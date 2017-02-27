3711382c53c90e002a96f686f2d37b7220696a81
HarryJackson1996 <harryjackson001@hotmail.co.uk>
2017-02-27 21:04:55 +0000

    Started to implement bounding box

--- src/BoundingBox.cc
+++ src/BoundingBox.cc
@@ -1,1 +1,66 @@
 #include "BoundingBox.h"
+
+BoundingBox::BoundingBox(const Vector3 centre, const float width, const float height, const float depth) {
+centre(make_shared<Vector3>(centre)),
+extent_x(make_shared<Vector3>(Vector3(width/2.0, 0.0, 0.0))),
+extent_y(make_shared<Vector3>(Vector3(0.0, height/2.0, 0.0))),
+extent_z(make_shared<Vector3>(Vector3(0.0, 0.0, depth/2.0))) {
+
+}
+
+SFBoundingBox::~SFBoundingBox() {
+  centre.reset();
+  extent_x.reset();
+  extent_y.reset();
+  extent_z.reset();
+}
+
+void BoundingBox::SetCentre(Vector3 & v) {
+  centre = make_shared<Vector3>(v);
+}
+
+bool straddles(const pair<float, float> & a, const pair<float, float> & b) {
+  if((a.first >= b.first && a.first <= b.second)
+     || (a.second >= b.first && a.second <= b.second))
+    {
+      return true;
+    }
+return false;
+}
+
+
+pair<float,float> BoundingBox::projectOntoAxis(const BoundingBox & b, enum AXIS axis) {
+  
+float lo, hi;
+
+  switch (axis) {
+  case X:
+    {
+      Vector3 bx = *b.x;
+      lo  = projection( Point3(Vector3(*(b.centre)) + (bx * -1)), HalfX());
+      hi  = projection( Point3(Vector3(*(b.centre)) + bx), HalfX());
+    }
+    break;
+  case Y:
+    {
+      Vector2 by = *b.y;
+      lo  = projection( Point2(Vector2(*(b.centre)) + (by * -1)), HalfY());
+      hi  = projection( Point2(Vector2(*(b.centre)) + by), HalfY());
+    }
+    break;
+  case Z:
+    {
+      Vector3 bz = *b.z;
+      lo  = projection( Point2(Vector2(*(b.centre)) + (bz * -1)), HalfZ());
+      hi  = projection( Point2(Vector2(*(b.centre)) + bz), HalfZ());
+    }
+    break;
+  }
+
+  return make_pair(lo, hi);
+}
+
+
+//implement BoundingBox::CollidesWith
+
+
--- src/BoundingBox.h
+++ src/BoundingBox.h
@@ -1,22 +1,43 @@
 #ifdef BOUNDINGBOX_H
 #define BOUNDINGBOX_H
 
+using namespace std;
+
+#include "SFMath.h"
 #include "GameAsset.h"
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
 #include <glm/mat4x4.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <memory>
+#include <utility>
+#include <ostream>
 
+enum AXIS {X, Y, Z};  			
 
 
 class BoundingBox : public GameAsset {
- public:
+ 
+public:
+	BoundingBox(const Vector3, 
+		    const float extent_x, 
+		    const float extent_y, 
+		    const float extent_z);
 	
+	virtual ~BoundingBox();
+	
+	void SetCentre(Vector3 &);
+	\bool CollidesWith(const shared_ptr<BoundingBox>);
 
+	glm::vec3 HalfX = glm::vec3(1.0, 0.0, 0.0);
+	glm::vec3 HalfY = glm::vec3(0.0, 1.0, 0.0);
+	glm::vec3 HalfZ = glm::vec3(0.0, 0.0, 1.0);
 
+private:
+	shared_ptr<Vector3> centre, extent_x, extent_y, extent_z;
 
+	pair<float,float> projectOntoAxis(const BoundingBox &, enum AXIS);
 
- private:
 };
 
 #endif 
